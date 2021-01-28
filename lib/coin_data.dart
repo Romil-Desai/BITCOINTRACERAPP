57935f24c48bf3a924a152524cda177db9cd022c
Romil-Desai <romildesai20000@gmail.com>
2021-01-28 16:58:12 +0530

    adding files

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,5 @@
+import 'package:bitcoin_ticker/network.dart';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +30,20 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  String selectedcurrency;
+  CoinData(this.selectedcurrency);
+
+  Future<List<String>> getprice() async {
+    List<String> currentrates = [];
+    for (String cryptotype in cryptoList) {
+      var url =
+          'http://api.coinlayer.com/api/live?access_key=37024f61e5e2ab5c6f85215cf2e1ff2a&target=$selectedcurrency&symbols=$cryptotype';
+      NetworkHelper networkHelper = NetworkHelper(url);
+      var pricedata = await networkHelper.getData();
+      double temp = pricedata["rates"][cryptotype];
+      currentrates.add(temp.toStringAsFixed(2));
+    }
+    return currentrates;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +7,65 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedcurrency = "USD";
+  List<String> currentprice = [" ", " ", " "];
+
+  List<DropdownMenuItem> getItems() {
+    List<DropdownMenuItem> dropdownitemlist = [];
+    for (int i = 0; i < currenciesList.length; i++) {
+      DropdownMenuItem newitem = DropdownMenuItem(
+        child: Text(currenciesList[i]),
+        value: currenciesList[i],
+      );
+      dropdownitemlist.add(newitem);
+    }
+    return dropdownitemlist;
+  }
+
+  void updateUI() async {
+    CoinData coinData = CoinData(selectedcurrency);
+    List<String> temp = await coinData.getprice();
+    setState(() {
+      currentprice = temp;
+    });
+  }
+
+  List<Widget> getcardlist() {
+    List<Widget> cardlist = [];
+    for (int i = 0; i < cryptoList.length; i++) {
+      Widget newitem = Padding(
+        padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+        child: Card(
+          color: Colors.lightBlueAccent,
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+            child: Text(
+              '1 ${cryptoList[i]} =${currentprice[i]} $selectedcurrency',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      );
+      cardlist.add(newitem);
+    }
+    return cardlist;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    updateUI();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,34 +76,28 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: getcardlist(),
             ),
           ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: DropdownButton(
+                value: selectedcurrency,
+                items: getItems(),
+                onChanged: (value) {
+                  setState(() {
+                    currentprice = [" ", " ", " "];
+                    selectedcurrency = value;
+                    updateUI();
+                  });
+                },
+              )),
         ],
       ),
     );
